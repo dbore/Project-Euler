95cae28c80a8b6562e7aa9424769a2e5633f933e
dbore <damik71@o2.pl>
2017-09-20 15:36:01 +0100

    Square root digital expansion Problem 80

---
+++ Square_root_digital_exp_prob80/SquareRoot/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+  
+            Console.WriteLine("Square root algorithms tests:\n");
+
+             Sqrt sqrt_obj = new Sqrt();                                                      //passed
+             double a;
+             double epsilon = 0.000000000001;
+             double epsilon_2 = 0.0000000001;
+             Stopwatch timer = new Stopwatch();
+
+             
+
+             Console.WriteLine("Testing method: math_sqrt");
+             a = 0.05; 
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 1.45;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 2;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt(a));              // -ok
+             a = 5;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt(a));              // -ok
+             a = 10;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt(a));              // -ok
+             a = 10.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 105.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 1089;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 10514;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 20449;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 23457;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 123456;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 288340;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+             a = 622521;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt(a));                // -ok
+
+
+             Console.WriteLine();
+             Console.WriteLine("Testing method: math_sqrt2"); // argument in format _._ produces wrong output for this method!!!
+             a = 0.05;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 1.45;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 2;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt2(a));            // -ok
+             a = 5;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt2(a));            // -ok
+             a = 10;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt2(a));            // -ok
+             a = 10.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 105.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 1089;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 10514;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 20449;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 23457;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 123456;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 288340;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+             a = 622521;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt2(a));              // -ok
+
+
+             Console.WriteLine();
+             Console.WriteLine("Testing method: math_sqrt3"); 
+             a = 0.05;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 1.45;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 2;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));   // -ok
+             a = 5;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));   // -ok
+             a = 10;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));   // -ok
+             a = 10.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 105.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 1089;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 10514;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 20449;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 23457;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 123456;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 288340;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok
+             a = 622521;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt3(a, epsilon));     // -ok 
+
+
+             Console.WriteLine();
+             Console.WriteLine("Testing method: math_sqrt4"); //algorithm is less accurate
+             a = 0.05;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 1.45;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 2;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));   // -ok
+             a = 5;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));   // -ok
+             a = 10;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));   // -ok
+             a = 10.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 105.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 1089;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 10514;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 20449;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 23457;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 123456;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon));     // -ok
+             a = 288340;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon_2));     // -this works with less precision
+             a = 622521;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt4(a, epsilon_2));     // -this works with less precision
+
+
+             Console.WriteLine();
+             Console.WriteLine("Testing method: math_sqrt5"); // This algorithm is not accurate and NOT RECOMMENDED to use!
+             a = 0.05;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -failed
+             a = 1.45;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -not accurate
+             a = 2;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt5(a));   // -not accurate
+             a = 5;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt5(a));   // -not accurate
+             a = 10;
+             Console.WriteLine("a = " + a + "\t\t√a = " + sqrt_obj.math_sqrt5(a));   // -not accurate
+             a = 10.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -not accurate
+             a = 105.14;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -not accurate
+             a = 1089;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -not accurate
+             a = 10514;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -not accurate
+             a = 20449;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -not accurate
+             a = 23457;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -ok
+             a = 123456;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -wrong
+             a = 288340;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -wrong
+             a = 622521;
+             Console.WriteLine("a = " + a + "\t√a = " + sqrt_obj.math_sqrt5(a));     // -wrong
+
+
+             Console.WriteLine();
+             Console.WriteLine("Project Euler - problem 80");
+             Console.WriteLine("Square root digital expansion");
+             int j = 1;
+             int result = 0;
+             timer.Start();
+             for (int i = 1; i <= 100; i++)
+             {
+                 
+                 if (j * j == i)
+                 {
+                     j++;
+                     continue;
+                 }
+                 String s = sqrt_obj.math_sqrt2_2(i, 100);
+                 s = s.Remove(s.IndexOf('.'), 1);
+                 s = s.Substring(0, 100);
+                 result = result + s.Sum(c => c - '0');
+
+             }
+             timer.Stop();
+             Console.WriteLine("The answer to this problem is: " + result);
+             Console.WriteLine("Calculation time : " + timer.ElapsedMilliseconds + " ms");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Square_root_digital_exp_prob80/SquareRoot/Sqrt.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace SquareRoot
+{
+    class Sqrt
+    {
+        public Sqrt() { }
+
+        //methods
+        public double math_sqrt(double number){
+            return Math.Sqrt(number);
+        }
+
+        public double math_sqrt2(double number)
+        {
+            //devide number into groups
+            int precision = 14;
+            string result_out = "";
+            string product = "";
+            List<string> groups = new List<string>();
+            string num_str = Convert.ToString(number);
+            int comma_pos = 0;      
+            if (num_str.Contains("."))
+            {
+                comma_pos = num_str.IndexOf(".");
+                num_str = num_str.Remove(comma_pos, 1);
+            }
+            if (num_str.Length % 2 == 0)
+            {
+                //devides into groups equaly
+              for (int i = 0; i < num_str.Length; i += 2)
+                        groups.Add(num_str.Substring(i, 2));
+          
+            }
+            else
+            {
+                //devides into groups not equaly
+                groups.Add(num_str.Substring(0, 1));
+                for (int i = 1; i < num_str.Length; i += 2)
+                    groups.Add(num_str.Substring(i, 2));
+            }
+            for(int i = 0; i< precision; i++)
+                groups.Add("00");
+ 
+            int group_pos = 0;
+            int group_digit = int.Parse(groups[group_pos]);
+            //look for number which power of 2 is less than the group_digit
+            int power = 0;
+            for (int i = 0; i <= group_digit; i++)
+                if (i * i <= group_digit)
+                    power = i;
+            Int64 r = group_digit - (power * power);
+            result_out = power.ToString();
+            product = product + power.ToString();
+
+        
+            while (true)
+            {
+                //insert comma in right place
+                if ((result_out.Length == comma_pos - 1 || result_out.Length == comma_pos)  && !result_out.Contains("."))
+                    result_out = result_out + ".";
+
+                group_pos++;
+                if (group_pos < groups.Count)
+                {
+                    if (groups[group_pos].Equals("00"))
+                    {
+                        if (!result_out.Contains("."))
+                            result_out = result_out + ".";
+                      
+                        r = Int64.Parse(Convert.ToString(r) + Convert.ToString(groups[group_pos]));
+               
+                    }
+                    else
+                    {
+                        r = Int64.Parse(Convert.ToString(r) + Convert.ToString(groups[group_pos]));
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+
+                         
+                Int64 k = 2 * Int64.Parse(product);
+                Int64 num_g = 0;
+                Int64 num_r = 0;
+                int i;
+                for (i = 9; i >= 0; i--)
+                {
+                    num_g = Int64.Parse(Convert.ToString(k) + Convert.ToString(i));
+                    if (num_g * i <= r)
+                    {
+                        result_out = result_out + i.ToString();
+                        product = product + i.ToString();
+                        num_r = num_g * i;
+                        break;
+                    }
+                }
+
+             
+                r = r - num_r;
+        
+            
+            }
+       
+           
+            return double.Parse(result_out);
+        }
+
+        public string math_sqrt2_2(double number, int precision)
+        {
+            //method to calculate square roots with greater percision, for example 100 decimal places
+            //devide number into groups
+            string result_out = "";
+            string product = "";
+            List<string> groups = new List<string>();
+            string num_str = Convert.ToString(number);
+            int comma_pos = 0;
+            if (num_str.Contains("."))
+            {
+                comma_pos = num_str.IndexOf(".");
+                num_str = num_str.Remove(comma_pos, 1);
+            }
+            if (num_str.Length % 2 == 0)
+            {
+                //devides into groups equaly
+                for (int i = 0; i < num_str.Length; i += 2)
+                    groups.Add(num_str.Substring(i, 2));
+
+            }
+            else
+            {
+                //devides into groups not equaly
+                groups.Add(num_str.Substring(0, 1));
+                for (int i = 1; i < num_str.Length; i += 2)
+                    groups.Add(num_str.Substring(i, 2));
+            }
+            for (int i = 0; i < precision; i++)
+                groups.Add("00");
+
+            int group_pos = 0;
+            int group_digit = int.Parse(groups[group_pos]);
+            //look for number which power of 2 is less than the group_digit
+            int power = 0;
+            for (int i = 0; i <= group_digit; i++)
+                if (i * i <= group_digit)
+                    power = i;
+           BigInteger r = group_digit - (power * power);
+            result_out = power.ToString();
+            product = product + power.ToString();
+
+
+            while (true)
+            {
+                //insert comma in right place
+                if ((result_out.Length == comma_pos - 1 || result_out.Length == comma_pos) && !result_out.Contains("."))
+                    result_out = result_out + ".";
+
+                group_pos++;
+                if (group_pos < groups.Count)
+                {
+                    if (groups[group_pos].Equals("00"))
+                    {
+                        if (!result_out.Contains("."))
+                            result_out = result_out + ".";
+
+                        r = BigInteger.Parse(Convert.ToString(r) + Convert.ToString(groups[group_pos]));
+
+                    }
+                    else
+                    {
+                        r = BigInteger.Parse(Convert.ToString(r) + Convert.ToString(groups[group_pos]));
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+
+
+                BigInteger k = 2 * BigInteger.Parse(product);
+                BigInteger num_g = 0;
+                BigInteger num_r = 0;
+                int i;
+                for (i = 9; i >= 0; i--)
+                {
+                    num_g = BigInteger.Parse(Convert.ToString(k) + Convert.ToString(i));
+                    if (num_g * i <= r)
+                    {
+                        result_out = result_out + i.ToString();
+                        product = product + i.ToString();
+                        num_r = num_g * i;
+                        break;
+                    }
+                }
+
+                r = r - num_r;
+
+            }
+
+
+            return result_out;
+        }
+
+        public double math_sqrt3(double number, double epsilon)
+        {
+          //square root algorithm: Newton-Raphson
+         
+            double a = number / 2;
+
+            while (Math.Abs(number / a - a) > epsilon)
+            {
+                a = (number / a + a) / 2;
+            }
+
+            return a;
+
+        }
+
+        public double math_sqrt4(double number, double epsilon)
+        {
+            //Bisection method
+
+            double max = number + 1;
+            double min = 0;
+            int i, j;
+
+            //get max
+            for (i = 0; i < number; i++)
+              if (i * i > number)
+                {
+                    max = i;
+                    break;
+                }
+
+            //get min
+             for (j = i; j > 0; j--)
+                if (j * j < number)
+                {
+                    min = j;
+                    break;
+                }
+
+
+            double a = ((max + min) / 2) * ((max + min) / 2);
+            while (Math.Abs(a - number) > epsilon)
+            {
+                 a = ((max + min) / 2) * ((max + min) / 2);
+                
+                if (a < number)
+                    min = ((max + min) / 2);
+                else
+                    max = ((max + min) / 2);
+              
+            }
+            return ((max + min) / 2);
+
+     
+        }
+
+        public double math_sqrt5(double number)
+        {
+            //Bakhshali approximation
+            int n;
+            double d;
+            double P;
+            double A;
+
+            for (n = (int) number; n > 0; n--)
+                if (n * n < number)
+                    break;
+
+            d = number - (n * n);
+            P = d / (2 * n);
+            A = n + P;
+
+            double S = A - ((P * P) / (2 * A));
+
+            return S; // S ≈ sqrt(number)
+        }
+
+    }
+
+
+
+}
